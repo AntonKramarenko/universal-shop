0eebddad5f157c808fa0edc13b71192de78d53ab
Anton <kramarenko4362@gmail.com>
2022-08-15 10:59:46 +0300

    add graph-ql db

---
+++ graphql-db/README.md
@@ -0,0 +1,3 @@
+1. Install dependencies
+2. Build the project - `yarn build`
+3. Start the project - `yarn start`
--- universal-shop/src/constants/urls.ts
+++ universal-shop/src/constants/urls.ts
@@ -1,1 +1,1 @@
-export const REQUEST_URL = 'http://localhost:4000/'
+export const REQUEST_URL = 'http://localhost:4000/';
